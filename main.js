f00b7b0531cf8b32b0589294f2114070f0735da6
Zhang Xin <zhangxindev@gmail.com>
2021-05-06 07:36:59 +0800

    兼容移动设备

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/***此处的 x y 均代表触摸或者点击处的坐标***/
+
+
 const cavans = document.querySelector('#pannel')
 // 获取渲染上下文
 const context = cavans.getContext('2d')
@@ -6,20 +9,21 @@ const context = cavans.getContext('2d')
 const eraser = document.querySelector('#eraser')
 const pen = document.querySelector('#pen')
 
+// 重置画布尺寸
 autoSetCavansSize(cavans)
 
-listenToMouse(cavans)
+// 响应用户动作
+listenToUser(cavans)
 
 
+// 设置一些全局状态
+let isWorking = false
+let isEraserWorking = false
+let oldPoint = { x: undefined, y: undefined }
 
-
-
-
-function listenToMouse(cavans) {
+function listenToUser(cavans) {
 
   // 设置画笔和橡皮是否工作
-  let isWorking = false
-  let isEraserWorking = false
 
   eraser.addEventListener('click', () => {
     isEraserWorking = !isEraserWorking
@@ -33,42 +37,49 @@ function listenToMouse(cavans) {
     document.querySelector('.switch-stationery').classList.add('toggle')
   })
 
+  // 设置起点
 
-  // 设置画线或者橡皮擦的起点
-  let oldPoint = { x: undefined, y: undefined }
-  cavans.addEventListener('mousedown', (event) => {
-    isWorking = true
-    let x = event.clientX
-    let y = event.clientY
+  if (document.ontouchstart !== undefined) {
 
-    oldPoint.x = x
-    oldPoint.y = y
+    // 触屏设备逻辑
+    cavans.addEventListener('touchstart', (event) => {
 
-    // 橡皮擦逻辑
-    if (isEraserWorking) {
-      context.clearRect(x - 8, y - 8, 10, 10)
-    }
+      let x = event.touches[0].clientX
+      let y = event.touches[0].clientY
 
-  })
+      clickOrTouch(x, y)
+    })
+
+    cavans.addEventListener('touchmove', (event) => {
+      let x = event.touches[0].clientX
+      let y = event.touches[0].clientY
+
+      startDraw(x, y)
+    })
+
+    cavans.addEventListener('touchend', () => {
+      isWorking = false
+    })
+
+  } else {
+    // 非触屏设备逻辑
+    // 设置画线或者橡皮擦的起点
+
+    cavans.addEventListener('mousedown', (event) => {
+      let x = event.clientX
+      let y = event.clientY
+
+      clickOrTouch(x, y)
+    })
+  }
+
 
   cavans.addEventListener('mousemove', (event) => {
 
     let x = event.clientX
     let y = event.clientY
-    let newPoint = {
-      x,
-      y
-    }
+    startDraw(x, y)
 
-    if (isWorking) {
-      if (isEraserWorking) {
-        context.clearRect(x - 8, y - 8, 16, 16)
-      } else {
-        context.lineWidth = 5
-        drawLine(oldPoint.x, oldPoint.y, newPoint.x, newPoint.y)
-        oldPoint = newPoint
-      }
-    }
   })
 
   cavans.addEventListener('mouseup', () => {
@@ -76,8 +87,6 @@ function listenToMouse(cavans) {
   })
 
 }
-
-
 
 
 /******画板尺寸重置******/
@@ -106,3 +115,36 @@ function drawLine(x1, y1, x2, y2) {
   context.lineTo(x2, y2);     // 终点
   context.stroke()
 }
+
+
+/***按下鼠标或触屏时逻辑***/
+
+function clickOrTouch(x, y) {
+  isWorking = true
+  oldPoint.x = x
+  oldPoint.y = y
+
+  // 橡皮擦逻辑
+  if (isEraserWorking) {
+    context.clearRect(x - 8, y - 8, 10, 10)
+  }
+}
+
+/***鼠标或手指移动的逻辑***/
+function startDraw(x, y) {
+  let newPoint = {
+    x,
+    y
+  }
+
+  if (isWorking) {
+    if (isEraserWorking) {
+      context.clearRect(x - 8, y - 8, 16, 16)
+    } else {
+      context.lineWidth = 5
+      drawLine(oldPoint.x, oldPoint.y, newPoint.x, newPoint.y)
+      oldPoint = newPoint
+    }
+  }
+}
+
